46117c16b9c6ac8cc43d4c8350c956aa4fbfce68
Rashita <rrashita_be19@thapar.edu>
2025-03-05 17:50:47 +0530

    promise methods added

--- polyfills/promise/PromiseAll.js
+++ polyfills/promise/PromiseAll.js
@@ -1,25 +1,58 @@
 function promiseAll(iterable) {
-    return new Promise((resolve, reject) => {
-      const results = [];
-      let resolvedCount = 0;
-      iterable.forEach((promise, index) => {
-        Promise.resolve(promise)
-          .then((value) => {
-            results[index] = value;
-            resolvedCount++;
-  
-            if (resolvedCount === iterable.length) {
-              resolve(results);
-            }
-          })
-          .catch(reject);
-      });
-      if (iterable.length === 0) {
-        resolve(results);
+  return new Promise((resolve, reject) => {
+    const results = [];
+    let resolvedCount = 0;
+    iterable.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((value) => {
+          results[index] = value;
+          resolvedCount++;
+
+          if (resolvedCount === iterable.length) {
+            resolve(results);
+          }
+        })
+        .catch(reject);
+    });
+    if (iterable.length === 0) {
+      resolve(results);
+    }
+  });
+}
+
+// IMPROVED:
+async function promiseAll(iterable) {
+  // Array to store resolved values
+  const results = [];
+  let resolvedCount = 0;
+
+  // If the iterable is empty, resolve immediately with an empty array
+  if (iterable.length === 0) {
+    return Promise.resolve(results);
+  }
+
+  // Return a new promise
+  return new Promise((resolve, reject) => {
+    // Iterate over the iterable
+    iterable.forEach(async (promise, index) => {
+      try {
+        // Await the promise and store the resolved value
+        const value = await Promise.resolve(promise);
+        results[index] = value; // Store the value at the correct index
+        resolvedCount++;
+
+        // If all promises have resolved, resolve the outer promise
+        if (resolvedCount === iterable.length) {
+          resolve(results);
+        }
+      } catch (error) {
+        // If any promise rejects, reject the outer promise
+        reject(error);
       }
     });
-  }
-  
+  });
+}
+
 // CASE I:
 // const p0 = Promise.resolve(3);
 // const p1 = 42;
@@ -30,7 +63,6 @@ function promiseAll(iterable) {
 // });
 
 // console.log(await promiseAll([p0, p1, p2])) // [3, 42, 'foo']
-
 
 // CASE II:
 // const p0 = Promise.resolve(30);
--- polyfills/promise/PromiseAny.js
+++ polyfills/promise/PromiseAny.js
@@ -1,29 +1,61 @@
 function promiseAny(iterable) {
-    return new Promise((resolve, reject) => {
-      const errors = [];
-      let rejectedCount = 0;
-  
-      iterable.forEach((promise, index) => {
-        Promise.resolve(promise)
-          .then((val) => {
-            resolve(val);
-          })
-          .catch((e) => {
-            rejectedCount++;
-            errors[index] = e;
-  
-            if (rejectedCount === iterable.length) {
-              reject(new AggregateError(errors, "All promises were rejected"));
-            }
-          });
-      });
-  
-      if (iterable.length === 0) {
-        return reject(new AggregateError([], "All promises were rejected"));
+  return new Promise((resolve, reject) => {
+    const errors = [];
+    let rejectedCount = 0;
+
+    iterable.forEach((promise, index) => {
+      Promise.resolve(promise)
+        .then((val) => {
+          resolve(val);
+        })
+        .catch((e) => {
+          rejectedCount++;
+          errors[index] = e;
+
+          if (rejectedCount === iterable.length) {
+            reject(new AggregateError(errors, "All promises were rejected"));
+          }
+        });
+    });
+
+    if (iterable.length === 0) {
+      return reject(new AggregateError([], "All promises were rejected"));
+    }
+  });
+}
+
+// IMPROVED:
+async function promiseAny(iterable) {
+  // Array to store rejection errors
+  const errors = [];
+  let rejectedCount = 0;
+
+  // If the iterable is empty, reject immediately with an AggregateError
+  if (iterable.length === 0) {
+    return Promise.reject(new AggregateError([], "All promises were rejected"));
+  }
+
+  // Return a new promise
+  return new Promise((resolve, reject) => {
+    // Iterate over the iterable
+    iterable.forEach(async (promise, index) => {
+      try {
+        // Await the promise and resolve the outer promise with its value
+        const value = await Promise.resolve(promise);
+        resolve(value);
+      } catch (error) {
+        // If the promise rejects, store the error and increment the rejected count
+        errors[index] = error;
+        rejectedCount++;
+
+        // If all promises have rejected, reject the outer promise with an AggregateError
+        if (rejectedCount === iterable.length) {
+          reject(new AggregateError(errors, "All promises were rejected"));
+        }
       }
     });
-  }
-  
+  });
+}
 
 //   Case I:
 // const p0 = Promise.resolve(42);
@@ -35,17 +67,16 @@ function promiseAny(iterable) {
 
 // console.log(await promiseAny([p0, p1])); // 42
 
-
 // Case II:
-const p0 = new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(42);
-    }, 100);
-  });
-  const p1 = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      reject('Err!');
-    }, 400);
-  });
-  
-  console.log(await promiseAny([p0, p1])); // 42
+// const p0 = new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(42);
+//     }, 100);
+//   });
+//   const p1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       reject('Err!');
+//     }, 400);
+//   });
+
+//   console.log(await promiseAny([p0, p1])); // 42
